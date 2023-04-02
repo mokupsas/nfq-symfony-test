dd9700879595310bab9f5e22b1b9931db84d8809
mokupsas <mockus.ri@gmail.com>
2023-04-03 01:14:46 +0300

    added readTime function to Twig

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,9 +7,15 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class IndexController extends AbstractController
 {
+    public function __construct(Environment $twig)
+    {
+        $twig->addFunction(new \Twig\TwigFunction('readTime','App\Common\Algo::countArticleReadTime'));
+    }
+
     #[Route('/', name: 'home')]
     public function list(ArticleRepository $articleRepository): Response
     {
--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -6,9 +6,15 @@ use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ViewController extends AbstractController
 {
+    public function __construct(Environment $twig)
+    {
+        $twig->addFunction(new \Twig\TwigFunction('readTime','App\Common\Algo::countArticleReadTime'));
+    }
+
     #[Route('/article/{id}', name: 'article_view')]
     public function view(Article $article): Response
     {
