b67c6304869327474ba86df516a97368d385353d
mokupsas <mockus.ri@gmail.com>
2023-04-03 01:05:17 +0300

    created SortArticles class

---
+++ src/Common/SortArticles.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Sort;
+
+class SortArticles
+{    
+    /**
+     * Sorts array of articles desc by updatedAt field
+     *
+     * @param  array $articles array of articles
+     * @return araay array sorted desc by updatedAt field
+     */
+    public static function sortByUpdatedAt($articles) : array
+    {
+        $updatedAtArray = array(); // array of updatedAt field values
+
+        // Adding updatedAt field values to $updatedAtArray 
+        foreach($articles as $key => $article)
+        {
+            $updatedAtArray[$key] = $article->getUpdatedAt();
+        }
+       
+        // Sorting $articles by $updatedAtArray desc values
+        array_multisort($updatedAtArray, SORT_DESC, $articles);
+        
+        return $articles;
+    }
+}
