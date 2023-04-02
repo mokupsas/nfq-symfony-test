d7bcde32be9fc031d200419f2f9630492b3b6520
mokupsas <mockus.ri@gmail.com>
2023-04-03 02:27:05 +0300

    updated ApplicationTest class

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -4,6 +4,37 @@ use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
+    public function testSortByUpdatedAt() 
+    {
+        $article0 = (new \App\Entity\Article)->setUpdatedAt(null);
+        $article1 = (new \App\Entity\Article)->setUpdatedAt(new DateTime('2023-04-01 12:15:13'));
+        $article2 = (new \App\Entity\Article)->setUpdatedAt(new DateTime('2023-04-02 22:15:13'));
+        $article3 = (new \App\Entity\Article)->setUpdatedAt(new DateTime('2023-04-02 23:00:00'));
+        $article4 = (new \App\Entity\Article)->setUpdatedAt(null);
+
+        $expectedArray = array();
+        array_push($expectedArray,
+            $article3,
+            $article2,
+            $article1,
+            $article0,
+            $article4
+        );
+
+        $articles = array();
+        array_push($articles,
+            $article1,
+            $article2,
+            $article3,
+            $article4,
+            $article0
+        );
+
+        $sortArticles = new \App\Common\SortArticles;
+        $result = $sortArticles->sortByUpdatedAt($articles);
+
+        $this->assertEquals($expectedArray, $result);
+    }
 
     public function testCountArticleReadTime() 
     {
